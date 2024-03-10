67a0908aaacfca224b38e770f5742e9ea5c613be
Dilip3040 <dilipkondiboyina3040@gmail.com>
2024-03-10 14:10:14 +0530

    User Test Case Modified

--- BookReview Backend/BookReview/src/test/java/com/sample/demo/UserTest.java
+++ BookReview Backend/BookReview/src/test/java/com/sample/demo/UserTest.java
@@ -27,9 +27,9 @@ public class UserTest {
 	@Test
 	public void aAddUserTest() {
 		User user = new User();
-		user.setEmail("patil@gmail.com");
-		user.setName("Patil");
-		user.setPassword("Patil@123");
+		user.setEmail("Sammy@gmail.com");
+		user.setName("Sammy");
+		user.setPassword("Sammy@2320");
 		boolean isAdded = userSevice.addUser(user);
 		assertTrue(isAdded);
 	}
@@ -38,9 +38,9 @@ public class UserTest {
 	@Test
 	public void bAddUserTest() {
 		User user = new User();
-		user.setEmail("patil@gmail.com");
-		user.setName("Patil");
-		user.setPassword("Patil@123");
+		user.setEmail("Sammy@gmail.com");
+		user.setName("Sammy");
+		user.setPassword("Sammy@2320");
 		boolean isAdded = userSevice.addUser(user);
 		assertFalse(isAdded);
 	}
@@ -48,9 +48,9 @@ public class UserTest {
 	@Test
 	public void cUpdateUserTest() {
 		User user = new User();
-		user.setEmail("patil@gmail.com");
-		user.setName("Patil");
-		user.setPassword("Patil@1234");
+		user.setEmail("Sammy@gmail.com");
+		user.setName("Sammy");
+		user.setPassword("Sammy@2320");
 		boolean isAdded = userSevice.updateUser(user);
 		assertTrue(isAdded);
 	}
@@ -58,42 +58,42 @@ public class UserTest {
 	// successful search
 	@Test
 	public void dGetUserByEmailTest() {
-		Optional<User> user = userSevice.getUser("patil@gmail.com");
+		Optional<User> user = userSevice.getUser("Sammy@gmail.com");
 		assertNotNull(user);
 	}
 
 	// failed search
 	@Test
 	public void eGetUserByEmailTest() {
-		Optional<User> user = userSevice.getUser("patilmoro@gmail.com");
+		Optional<User> user = userSevice.getUser("Sammydil@gmail.com");
 		assertTrue(user.isEmpty());
 	}
 
 	// successful login
 	@Test
 	public void fValidUserLoginTest() {
-		boolean isAuthenticated = userSevice.validateUser("patil@gmail.com", "Patil@1234");
+		boolean isAuthenticated = userSevice.validateUser("Sammy@gmail.com", "Sammy@2320");
 		assertTrue(isAuthenticated);
 	}
 
 	// failed login
 	@Test
 	public void gValidUserLoginTest() {
-		boolean isAuthenticated = userSevice.validateUser("patil@gmail.com", "Patil@123");
+		boolean isAuthenticated = userSevice.validateUser("Sammy@gmail.com", "Sammy@230");
 		assertFalse(isAuthenticated);
 	}
 
 	// successful delete
 	@Test
 	public void hDeleteUserByEmail() {
-		boolean isDeleted = userSevice.deleteUser("patil@gmail.com");
+		boolean isDeleted = userSevice.deleteUser("Sammy@gmail.com");
 		assertTrue(isDeleted);
 	}
 
 	// failed delete
 	@Test
 	public void iDeleteUserByEmail() {
-		boolean isDeleted = userSevice.deleteUser("patil@gmail.com");
+		boolean isDeleted = userSevice.deleteUser("Sammy@gmail.com");
 		assertFalse(isDeleted);
 	}
 
